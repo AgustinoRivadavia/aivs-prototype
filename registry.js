7651faa107ce996a84e81712fb2d697af314202f
AgustinoRivadavia <agustin18p@gmail.com>
2026-01-05 10:47:31 -0300

    Add files via upload

---
+++ registry.js
@@ -0,0 +1,14 @@
+const registry = {
+  "AIVS-000001": {
+    name: "AITANA",
+    type: "Artificial Intelligence",
+    registered: "2025-01-10",
+    hash: "9f4c1a2b8e"
+  },
+  "AIVS-000002": {
+    name: "NOVA",
+    type: "Artificial Intelligence",
+    registered: "2025-02-03",
+    hash: "a83d91ff02"
+  }
+};
